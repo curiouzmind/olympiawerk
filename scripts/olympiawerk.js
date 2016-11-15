74c1d9099266491f9870cc4e5ed054ef75c55cac
Bolaji Alade <Bolaji Alade>
2016-11-15 19:25:32 +0000

    added fixes

--- scripts/olympiawerk.js
+++ scripts/olympiawerk.js
@@ -13,10 +13,22 @@ function contactMap() {
   $("a.btn-floating").text("View Map");
   $("a.btn-floating").click(function(e) {
     e.preventDefault();
-    $(this).text($(this).text()== "Close Map" ? "View Map" : "Close Map");
+    $(this).text($(this).text() == "Close Map" ? "View Map" : "Close Map");
     $(".page-banner").toggleClass("page-banner-state2","page-banner-state1").fadeIn('slow');
     $(".contact-map").toggleClass("contact-map-state1").fadeIn('slow');
   });
+  var amountScrolled = 250;
+
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+      $('a.btn-floating').fadeIn('fast');
+    } else {
+      $('a.btn-floating').fadeOut('fast');
+    }
+  });
+  // $('html, body').animate({
+  //     scrollTop: 0
+  // }, 900);
 }
 
 $(document).ready(function() {
