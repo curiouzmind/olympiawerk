273c3e82b9ed5bf826c71add0742f5c99d9bd06b
Bolaji Alade <Bolaji Alade>
2016-11-15 16:06:39 +0000

    added footer fixed to page bottom, reduced space between content and footer

--- scripts/olympiawerk.js
+++ scripts/olympiawerk.js
@@ -1,3 +1,9 @@
-"use strict"
+// "use strict"
 
-// $("footer").css("color","red");
+$(document).ready(function() {
+  $(".footer").css("margin-top",
+    $(document).height() -
+    ($(".header").height() + $(".content").height()
+    - $(".footer").height())
+  )
+});
