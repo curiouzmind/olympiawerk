8cdc786be15f61e5bc8f0e49d7c41068291fd617
Bolaji Alade <Bolaji Alade>
2016-12-05 00:28:58 +0000

    added woodworking and contact pages

--- js/olympiawerk.js
+++ js/olympiawerk.js
@@ -12613,6 +12613,26 @@ function playPause() {
       myVideo.pause();
   }
 }
+function contactMap() {
+  $(".page-banner").addClass("page-banner-state1");
+  $(".contact-map").addClass("contact-map-state1");
+  // $("a.btn-floating").text("View Map");
+  $(".page-banner").click(function(e) {
+    e.preventDefault();
+    // $(this).text($(this).text() == "Close Map" ? "View Map" : "Close Map");
+    $(".page-banner").toggleClass("page-banner-state2","page-banner-state1").fadeIn('slow');
+    $(".contact-map").toggleClass("contact-map-state1").fadeIn('slow');
+  });
+  var amountScrolled = 250;
+
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+      $('a.btn-floating').fadeIn('fast');
+    } else {
+      $('a.btn-floating').fadeOut('fast');
+    }
+  });
+}
 
 /*
 MAIN JS
